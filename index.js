24653f067f56649efc05d0ffe575cee902792c28
aramei <pasichnyk89@gmail.com>
2017-04-24 23:42:55 +0300

    some html

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ var express = require("express");
 var app = express();
 
 var path = require('path');
-var users = require('./users.json');
 var bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
@@ -14,12 +10,12 @@ app.post('/login', function(req, res) {
     var body = req.body;
 	var valid = false;
     
-    // for (var i=0; i <users.length; i++) {
-    //     if ((body.login === users[i].login) && (body.password === users[i].password)) {
-    //         valid = true;
-    //         break;
-    //     }
-    // }
+    for (var i=0; i <users.length; i++) {
+        if ((body.login === users[i].login) && (body.password === users[i].password)) {
+            valid = true;
+            break;
+        }
+    }
 
     res.send(valid);
 });
